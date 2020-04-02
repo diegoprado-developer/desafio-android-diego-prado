e2f38520a4e64f72c215588eb2cd7680ae6f9705
Diego Prado <diego.prado@capgemini.com.br>
2020-04-02 09:27:33 -0300

    adapter ok, mais permissao internet

--- app/src/main/java/com/diegoprado/desafio_android_diego_prado/MainActivity.kt
+++ app/src/main/java/com/diegoprado/desafio_android_diego_prado/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.diegoprado.desafio_android_diego_prado
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
+    var teste = Uri.parse("https://pbs.twimg.com/media/Dw_sw_FX0AQNi4h.jpg")
+    var list: MutableList<Person> = emptyList<Person>().toMutableList()
     lateinit var recyclerView: RecyclerView
     lateinit var adapter: PersonAdapter
 
@@ -13,7 +17,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        recyclerView = findViewById(R.id.rvPersonsHeros)
 
-        
+        list.add(Person(1, "MADERA", "MADERANTE MADERA", teste))
+
+        loadRecycle()
+    }
+
+    fun loadRecycle(){
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        adapter = PersonAdapter(list, this)
+        recyclerView.adapter = adapter
     }
 }
